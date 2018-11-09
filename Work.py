03446b282fdc80ae3d2c3dd70a5afc58e319b8ad
Phantom <ncwick@student.rtc.edu>
2018-11-09 00:22:47 -0800

    Added more code
    Added comments, code, and var assignments

--- Work.py
+++ Work.py
@@ -3,7 +3,8 @@
 # Group Project with PSwift
 # Nathan Wick, ncwick@student.rtc.edu/ncwick96@gmail.com
 #
-# Code referenced from: https://github.com/RTCedu/CNA330
+# Code referenced from: https://github.com/RTCedu/CNA330/JobHunter/JobHunter.py
+#                       https://github.com/RTCedu/CNA336/Spring2018/Sql.py
 #
 import mysql.connector
 import sys
@@ -12,18 +13,48 @@ import urllib.request
 import os
 import time
 
+conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='Computer_Parts')
+c = conn.cursor()
+c.execute('''CREATE TABLE "Computer_Parts" (id INT PRIMARY KEY AUTOINCREMENT, Date DATE, Class TEXT,
+Name TEXT, Model INT, Manufacturer TEXT, Quantity INT, Price FLOAT, Site TEXT);''')
 
-def connect_to_sql():
-    conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='Computer_Parts')
+c.execute("INSERT INTO Jobs(id, Date, Class, Name, Model, Manufacturer, Quantity, Price, Site)"""
+               " VALUES(0,'11/8/18', 'RAM', 'Corsair 64gb RAM 3200', '224873', 'Corsair', '64',""
+                '550.98', 'Amazon')
+c.close()
 
 
-def create_tables(cursor, Computer_Parts):
 
-    cursor.execute('''CREATE TABLE "Computer_Parts" (id INT PRIMARY KEY AUTOINCREMENT, Date DATE, Class TEXT,
-    Name TEXT, Model INT, Manufacturer TEXT, Quantity INT, Price FLOAT, Site TEXT);''')
+# {                 Planning on using these in boolean ops later, may delete if better method found
+# Classes for db table, and var assignment for later processing
+a = "RAM"
+b = "Heat_Sink"
+c = "CPU"
+d = "PSU"
+e = "Mother_Board"
 
-    cursor.execute("INSERT INTO Jobs(id, Date, Class, Name, Model, Manufacturer, Quantity, Price, Site)"""
-                   " VALUES(0,'11/8/18', 'RAM', 'Corsair 64gb RAM 3200', '224873', 'Corsair', '64',"
-                   " '550.98', 'Amazon') ;")
-    cursor.close()
-    return cursor
+# Sites for db table, and var assignment for later processing
+am = "Amazon"
+bb = "Best_Buy"
+f = "Frys"
+ne = "New_Egg"
+# }
+
+# Runs with exit code 0, no errors, but does not create DB, PSwift, compare with your JH script please.
+## Work in progress below
+
+counter = 0
+query = "#INSERT URL(S) HERE#"
+jsonpage = 0
+try:
+    contents = urllib.request.urlopen(query)
+    response = contents.read()
+    jsonpage = json.loads(response)
+except:
+    pass
+    time.sleep(1)
+data = json.dumps(jsonpage['data'])
+for i in data:
+    conn.c(
+        "INSERT INTO Computer_Parts(#VAR STUFF HERE#+#TABLE VALUES/DATA#)"
+    )
